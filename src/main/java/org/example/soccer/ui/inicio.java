fba3a6f21942939e8d57ca114f03a93b0e6158b1
RodrigoSBA10 <rodrigosba456@gmail.com>
2025-09-24 20:27:35 -0600

    Proyecto
    Creación y configuración del proyecto.

---
+++ src/main/java/org/example/soccer/ui/inicio.java
@@ -0,0 +1,17 @@
+package org.example.soccer.ui;
+import javax.swing.*;
+
+public class inicio {
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Inicio");
+        frame.setContentPane(new inicio().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(500,500);
+        frame.setTitle("Inicio");
+        frame.setVisible(true);
+
+    }
+}
