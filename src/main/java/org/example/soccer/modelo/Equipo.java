312385b412d40043fd1486ec3c611ee32c8d02bb
RodrigoSBA10 <rodrigosba456@gmail.com>
2025-09-27 22:06:57 -0600

    Equipo
    Se implemento el repositorio, servicios e interfaz de usuario para poder hacer el CRUD de la clase/entidad Equipo con su respectivo mapeo

---
+++ src/main/java/org/example/soccer/modelo/Equipo.java
@@ -0,0 +1,39 @@
+package org.example.soccer.modelo;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Equipos")
+public class Equipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+
+    public Equipo() {
+    }
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo " + nombre;
+    }
+}
